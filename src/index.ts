c6192c0b93bbd548c3183568c772252e503c6c04
inyong-e <jiy8319@gmail.com>
2023-11-11 16:44:12 +0900

    feat: eslint

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+console.log("123333");
+
+console.log("11123");
+
+interface Props {
+  name: string;
+  age: number;
+}
+
+const test: Props = {
+  name: "123",
+  age: 123,
+};
+
+console.log(test);
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.ts',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
@@ -10,15 +10,6 @@ module.exports = {
   plugins: [new HtmlWebpackPlugin({
     template: "./public/index.html"
   })],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        enforce: "pre",
-        use: ["source-map-loader"],
-      },
-    ],
-  },
   devServer: {
     static: {
       directory: path.resolve(__dirname, 'dist')
